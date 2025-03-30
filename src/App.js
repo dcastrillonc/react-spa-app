6a63fd68c01bf18cba1343ba27ea3fb9066ce4d0
Daniela Castrillón <d.castrillonc@uniandes.edu.co>
2025-03-29 21:57:16 -0500

    Commit inicial SPA React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//Importar componente que contiene la lógica para mostrar y filtrar las publicaciones
+import PostList from './containers/PostList'; 
 
-function App() {
+//Componente principal o raíz de la aplicación
+const App = () => {
   return (
     <div className="App">
+      {/* Encabezado de la aplicación */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Título principal de la página */}
+        <h1 className="display-4">Lista de publicaciones</h1>
       </header>
+      {/* Contenedor principal del contenido */}
+      <main className="container py-5">
+        {/* Renderizar el componente PostList que maneja la lógica y muestras las publicaciones */}
+        <PostList/>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/containers/PostList.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+//Hacer peticiones HTTP e interactuar con la API pública
+import axios from 'axios';
+//Importar componente para mostrar las publicaciones
+import PostCard from '../components/PostCard';
+
+const PostList = () => {
+  //Estado para almacenar todas las publicaciones
+  const [posts, setPosts] = useState([]);
+  //Estado para almacenar las publicaciones filtradas
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  //Estado para almacenar el término de búsqueda
+  const [searchTerm, setSearchTerm] = useState('');
+
+  //useEffect permite sincronizar un componente con la API y hacerle la petición
+  useEffect(() => {
+    //Realizar la petición GET a la API JSONPlaceholder para obtener las publicaciones
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        //Guardar todas las publicaciones y las filtradas en el estado correspondiente
+        setPosts(response.data);
+        setFilteredPosts(response.data);
+      })
+      .catch(error => {
+        //Manejo de errores en caso de que la petición falle
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  //Función para manejar el cambio en la sección de búsqueda para el filtrado
+  const handleSearch = (event) => {
+    //Actualizar el término de búsqueda
+    setSearchTerm(event.target.value); 
+    const filtered = posts.filter(post =>
+      //Filtrar por título
+      post.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    //Actualizar el estado con las publicaciones filtradas
+    setFilteredPosts(filtered);
+  };
+
+  return (
+    <div>
+      {/*Campo de búsqueda para filtrar las publicaciones por título en tiempo real*/}
+      <input
+        type="text"
+        className="form-control mb-4 shadow-sm"
+        placeholder="Filtrar por título..."
+        value={searchTerm}
+        onChange={handleSearch} //Llamar a la función cuando se escriba
+      />
+      {/*Muestras las publicaciones filtradas*/}
+      {filteredPosts.map(post => (
+        //Renderizar cada publicación
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
